5ddc1cf47beea519f10bc9f10e04bfd14e0f2e01
JoselynDRF <mariajoselyndosramos@yahoo.es>
2019-08-16 21:36:52 +0100

    Create gems controller

---
+++ Assets/Scripts/Gems.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Gems : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			Destroy(gameObject);
+		}
+	}
+}
