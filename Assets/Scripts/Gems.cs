c3394ebc3ca90a98e42e3341ee602dc84a530c42
JoselynDRF <mariajoselyndosramos@yahoo.es>
2019-08-17 16:50:41 +0100

    Create score

--- Assets/Scripts/Gems.cs
+++ Assets/Scripts/Gems.cs
@@ -4,6 +4,7 @@ public class Gems : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.tag == "Player") {
+			Score.scoreValue += 100;
 			Destroy(gameObject);
 		}
 	}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+	private Text score;
+	public static int scoreValue = 0;
+
+	void Start() {
+		score = GetComponent<Text>();
+	}
+
+	void Update() {
+		score.text = "SCORE: " + scoreValue;
+	}
+}
