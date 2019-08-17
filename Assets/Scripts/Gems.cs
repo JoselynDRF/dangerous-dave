ceb2e306f830db2b14223ac4bdd445dbf7305e69
JoselynDRF <mariajoselyndosramos@yahoo.es>
2019-08-17 16:58:44 +0100

    Create red gem score

--- Assets/Scripts/Gems.cs
+++ Assets/Scripts/Gems.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 
 public class Gems : MonoBehaviour {
+	private int blueScore = 100;
+	private int redScore = 150;
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.tag == "Player") {
-			Score.scoreValue += 100;
+			Score.scoreValue += gameObject.tag == "RedGem" ? redScore : blueScore;
 			Destroy(gameObject);
 		}
 	}
