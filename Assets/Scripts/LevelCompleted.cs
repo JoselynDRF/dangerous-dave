dcd9e2cb3c06cc429227b19ce5c52c17fdbd9577
JoselynDRF <mariajoselyndosramos@yahoo.es>
2019-08-18 17:51:27 +0100

    Create Level Completed scene

---
+++ Assets/Scripts/LevelCompleted.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class LevelCompleted : MonoBehaviour {
+	private Animator animator;
+
+  void Start() {
+    animator = GetComponent <Animator>();
+  }
+
+	void FixedUpdate() {
+		transform.position = new Vector3(transform.position.x + 0.09f, transform.position.y, 0);
+		animator.SetFloat("velX", 1);
+		animator.SetBool("grounded", true);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+    string tag = other.gameObject.tag;
+    GoToNextLevel(tag);
+  }
+
+	void GoToNextLevel(string tag) {
+    if (tag == "Door") {
+      Debug.Log("NEXT LEVEL");
+    }
+  }
+}
