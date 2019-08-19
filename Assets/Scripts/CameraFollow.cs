ae9d3ecf2e8bd4a5c2aa07d5d561dc17ecc73880
JoselynDRF <mariajoselyndosramos@yahoo.es>
2019-08-19 18:12:21 +0100

    Create camera movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+  public GameObject player;
+	public Vector2 minCameraPosition;
+	public Vector2 maxCameraPosition;
+
+	void FixedUpdate() {
+		float cameraPositionX = Mathf.Clamp(player.transform.position.x, minCameraPosition.x, maxCameraPosition.x);
+		transform.position = new Vector3(cameraPositionX, transform.position.y, transform.position.z);
+	}
+}
