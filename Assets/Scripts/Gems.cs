1cd7ca1a1057fb2a46ba9ce16a62302ce3308fa9
JoselynDRF <mariajoselyndosramos@yahoo.es>
2019-08-31 18:12:09 +0100

    Update tiles

--- Assets/Scripts/Gems.cs
+++ Assets/Scripts/Gems.cs
@@ -4,6 +4,9 @@ public class Gems : MonoBehaviour {
 	private int purpleScore = 50;
 	private int blueScore = 100;
 	private int redScore = 150;
+	private int ringScore = 200;
+	private int greenScore = 250;
+	private int crownScore = 300;
 	private int trophyScore = 1000;
 
 	void OnTriggerEnter2D(Collider2D other) {
@@ -15,6 +18,15 @@ public class Gems : MonoBehaviour {
 					break;
 				case "RedGem":
 					GameManager.score += redScore;
+					break;
+				case "Ring":
+					GameManager.score += ringScore;
+					break;
+				case "GreenGem":
+					GameManager.score += crownScore;
+					break;
+				case "Crown":
+					GameManager.score += greenScore;
 					break;
 				case "Trophy":
 					GameManager.score += trophyScore;
