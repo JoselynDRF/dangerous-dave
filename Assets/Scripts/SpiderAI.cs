6993755a77609b57dcb58c207beead6d7671bdc1
JoselynDRF <mariajoselyndosramos@yahoo.es>
2019-09-01 14:47:49 +0100

    Create spider movement

---
+++ Assets/Scripts/SpiderAI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SpiderAI : MonoBehaviour {
+	public Transform[] spiderPoints;
+	private int currentIndex = 0;
+	private float speed = 4f;
+
+	void FixedUpdate() {
+		MoveSpider();
+	}
+	
+	void MoveSpider() {
+		transform.position = Vector2.MoveTowards(
+			transform.position,
+			spiderPoints[currentIndex].transform.position,
+			speed * Time.deltaTime
+		);
+
+		if (transform.position == spiderPoints[currentIndex].transform.position) currentIndex++;
+		if (currentIndex == spiderPoints.Length) currentIndex = 0;
+	}
+}
